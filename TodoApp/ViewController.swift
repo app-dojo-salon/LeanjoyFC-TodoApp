2f177ef5c13da47dfb73682f0e37dca0b00b70bf
koji-torishima <torikou3685@gmail.com>
2020-09-06 22:36:46 +0900

    BaseProjectsの作成

---
+++ TodoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by koji torishima on 2020/09/06.
+//  Copyright © 2020 LeanjoyFC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
