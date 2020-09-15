55de40c712de5a2ff42c0e74ce7d2735976ba5fd
TakumaOgawa <syuu.2531@gmail.com>
2020-09-16 00:48:32 +0900

    RealmSwiftをCocoapodsで導入

---
+++ TodoApp/Models/items.swift
@@ -0,0 +1,13 @@
+//
+//  items.swift
+//  TodoApp
+//
+//  Created by 小川卓馬 on 2020/09/16.
+//  Copyright © 2020 LeanjoyFC. All rights reserved.
+//
+
+import RealmSwift
+
+class items: Object {
+    @objc dynamic var item = ""
+}
--- TodoApp/Views/Controllers/ItemListViewController.swift
+++ TodoApp/Views/Controllers/ItemListViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 final class ItemListViewController: UIViewController {
     
