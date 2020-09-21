c33704fc3383459566066b3ea68a1e3175680ef6
koji-torishima <torikou3685@gmail.com>
2020-09-22 00:16:27 +0900

    IdentifierTypeを定義して、Identifierをまとめる

---
+++ TodoApp/Identifier/IdentifierType.swift
@@ -0,0 +1,15 @@
+//
+//  IdentifierType.swift
+//  TodoApp
+//
+//  Created by koji torishima on 2020/09/21.
+//  Copyright © 2020 LeanjoyFC. All rights reserved.
+//
+
+import Foundation
+
+enum IdentifierType: String {
+    case segueId = "unwindByItemAdd"
+    case cellId = "Cell"
+    case nibId = "ItemListTableViewCell"
+}
