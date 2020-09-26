91445037f6b1bb85898e601cedff04cfa23cd0a0
猫吉 <freeaddress834@gmail.com>
2020-09-26 10:14:01 +0900

    新規追加用のチェック項目を宣言、itemTextFieldで未入力時に出るアラートを追加、Add画面のitemListFieldをStoryBoardと紐付け

--- TodoApp/Views/Controllers/ItemAddViewController.swift
+++ TodoApp/Views/Controllers/ItemAddViewController.swift
@@ -12,9 +12,9 @@ import RealmSwift
 final class ItemAddViewController: UIViewController {
     
     @IBOutlet private weak var itemTextField: UITextField!
-    // テストデータ
-    // private修飾子をつけるために、varで定義
-    private(set) var testCheckItem = "test"
+    // 新規追加用のチェック項目
+    // まだ追加すると確定してないので、beta、を付けた
+    private(set) var betaCheckItem = CheckListItem()
         
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +22,14 @@ final class ItemAddViewController: UIViewController {
     }
     
     @IBAction func addCheckItem(_ sender: Any) {
+        // 1文字も入力されてなければ、アラートで警告し、処理を中断
+        if itemTextField.text!.isEmpty {
+            let alert = UIAlertController(title: "エラー", message: "1文字以上を入力してください", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            present(alert, animated: true, completion:  nil)
+            return
+        }
+        
         performSegue(withIdentifier: IdentifierType.segueId, sender: nil)
     }
     
