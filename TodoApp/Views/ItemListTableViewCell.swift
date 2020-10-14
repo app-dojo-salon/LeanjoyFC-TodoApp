044c663a0e8feed633c0169dad8dab73ccc42cd2
TakumaOgawa <syuu.2531@gmail.com>
2020-10-14 12:23:19 +0900

    ItemListVCのセル、ItemEditVCにUIを設置

--- TodoApp/Views/ItemListTableViewCell.swift
+++ TodoApp/Views/ItemListTableViewCell.swift
@@ -15,7 +15,7 @@ class ItemListTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
+
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
