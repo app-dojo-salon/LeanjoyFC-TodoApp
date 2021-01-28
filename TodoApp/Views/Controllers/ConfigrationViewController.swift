00dbbd4c899dd9b3ed26ec75fc63f20ffb56b91d
kodou <kodou.rhythm@gmail.com>
2021-01-29 07:46:13 +0900

    アプリバージョンを取得してラベル表示

--- TodoApp/Views/Controllers/ConfigrationViewController.swift
+++ TodoApp/Views/Controllers/ConfigrationViewController.swift
@@ -11,10 +11,11 @@ import UIKit
 class ConfigrationViewController: UIViewController {
     @IBOutlet weak var configrationContentsView: UIView!
     @IBOutlet weak var appVersionLabel: UILabel!
+    let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        appVersionLabel.text = version
         setConfigrationContentsView()
     }
     
